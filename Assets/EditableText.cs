b48a368177dd24682739bb05d77590578ff18fde
João Roberto de Souza Junior <joaogameprog@gmail.com>
2014-09-26 09:42:20 -0300

    Comentários adicionados

--- Assets/EditableText.cs
+++ Assets/EditableText.cs
@@ -9,11 +9,21 @@ using System.Linq;
 [RequireComponent (typeof(TextMesh))]
 public class EditableText : MonoBehaviour {
 	#region Fields
+
+	/// <summary>
+	/// The action is activated on enter pressed.
+	/// </summary>
 	public Action OnEnterPressed;
 
+	/// <summary>
+	/// The identifier.
+	/// </summary>
 	[SerializeField]
 	int id;
 
+	/// <summary>
+	/// The focused identifier.
+	/// </summary>
 	static int focusedId;
 	public static int FocusedId{
 		get
@@ -31,23 +41,49 @@ public class EditableText : MonoBehaviour {
 		}
 
 	}
+
+	/// <summary>
+	/// The next text.
+	/// </summary>
 	[SerializeField]
 	EditableText nextText;
 
+	/// <summary>
+	/// The color of the normal state.
+	/// </summary>
 	[SerializeField]
 	Color normalColor = Color.white;
+
+	/// <summary>
+	/// The color of the selected state.
+	/// </summary>
 	[SerializeField]
 	Color selectedColor = Color.blue;
 
+	/// <summary>
+	/// The tap gesture.
+	/// </summary>
 	TapGesture tapGesture;
 
+	/// <summary>
+	/// The text mesh.
+	/// </summary>
 	TextMesh textMesh;
 
+	/// <summary>
+	/// The box collider.
+	/// </summary>
 	BoxCollider2D boxCollider;
 
+	/// <summary>
+	/// The text.
+	/// </summary>
 	[SerializeField]
 	string text;
-
+	/// <summary>
+	/// Gets or sets the text and the textMesh.
+	/// </summary>
+	/// <value>The text.</value>
 	public string Text
 	{ 
 		get
@@ -61,31 +97,85 @@ public class EditableText : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// The text showed when the string is empty, 
+	/// by default is loaded with textMesh inspector text value.
+	/// </summary>
 	[SerializeField]
 	string defaultText;
 
+	#region Indexer
+	/// <summary>
+	/// Determines if the indexer character '|' will appear on the textMesh
+	/// </summary>
 	bool showIndexer;
+	/// <summary>
+	/// The indexer (in)visible change time.
+	/// </summary>
 	float indexerTime = 0.5f;
+
+	/// <summary>
+	/// The indexer timer.
+	/// </summary>
 	float indexerTimer = 0f;
+
+	/// <summary>
+	/// The indexer position.
+	/// </summary>
 	int indexerPos = 0;
-	string shownString = "";
-	int shownStartIndex;
-
-	bool isSelected;
-
-
-	TouchScreenKeyboard mobileKeyboard;
-
-	[SerializeField]
-	TouchScreenKeyboardType type;
-
-	[SerializeField]
-	bool isPassword;
 
 	#endregion
 
+	/// <summary>
+	/// The shown string.
+	/// </summary>
+	string shownString = "";
+
+	/// <summary>
+	/// The index of the shown text start.
+	/// </summary>
+	int shownStartIndex;
+
+	/// <summary>
+	/// Determines if the field is selected.
+	/// </summary>
+	bool isSelected;
+
+	/// <summary>
+	/// The mobile keyboard.
+	/// </summary>
+	TouchScreenKeyboard mobileKeyboard;
+
+	/// <summary>
+	/// The type filter.
+	/// </summary>
+	[SerializeField]
+	TouchScreenKeyboardType type;
+
+	/// <summary>
+	/// The field is a password?
+	/// </summary>
+	[SerializeField]
+	bool isPassword;
+
+	/// <summary>
+	/// Gets the width of the textMesh.
+	/// </summary>
+	/// <value>The width.</value>
+	public float width { get { return GetTextWidth(textMesh.text); } }
+
+	/// <summary>
+	/// Gets the height of the textMesh.
+	/// </summary>
+	/// <value>The height.</value>
+	public float height { get { return renderer.bounds.size.y; } }
+	
+	#endregion
+
 	#region Behaviours
-	// Use this for initialization
+	/// <summary>
+	/// Components initializations.
+	/// </summary>
 	void Awake () {
 		tapGesture = GetComponent<TapGesture> ();
 		textMesh = GetComponent<TextMesh> ();
@@ -96,7 +186,9 @@ public class EditableText : MonoBehaviour {
 		}
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Verify if a key is pressed and validate on text.
+	/// </summary>
 	void OnGUI () {
 		if(isSelected)
 		{
@@ -189,22 +281,18 @@ public class EditableText : MonoBehaviour {
 					}
 					shownString = textMesh.text;
 
-				/* 
-					if(Event.current != null){
-						if(Event.current.isKey){
-							Text = Event.current.keyCode.ToString();
-						}
-					}
-					*/
-
 			}
 		}
 	}
 
+	/// <summary>
+	/// Do verifications when selected.
+	/// </summary>
 	void Update()
 	{
 		if(isSelected)
 		{
+			// If clicked remove field selection
 			if(Input.GetMouseButtonDown(0))
 			{
 				var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -213,6 +301,8 @@ public class EditableText : MonoBehaviour {
 					Selected(this, System.EventArgs.Empty);
 				}
 			}
+
+			//Show | Hide Indexer
 			if(!isPassword && mobileKeyboard == null){
 				indexerTimer -= Time.deltaTime;
 
@@ -227,25 +317,30 @@ public class EditableText : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Raises the enable event.
+	/// Sign components events
+	/// </summary>
 	void OnEnable()
 	{
 		tapGesture.Tapped += Selected;
-
 	}
 
+	/// <summary>
+	/// Raises the disable event.
+	/// Unsign components events.
+	/// </summary>
 	void OnDisable()
 	{
 		tapGesture.Tapped -= Selected;
 		
 	}
-
-
-
-
 	#endregion
 
 	#region Events
-
+	/// <summary>
+	/// Raises when enter is pressed.
+	/// </summary>
 	void EnterPressed ()
 	{
 		if(OnEnterPressed != null)
@@ -255,6 +350,12 @@ public class EditableText : MonoBehaviour {
 			Selected (this, System.EventArgs.Empty);
 		}
 	}
+
+	/// <summary>
+	/// Selected toggle
+	/// </summary>
+	/// <param name="sender">Sender.</param>
+	/// <param name="e">E.</param>
 	public void Selected (object sender, System.EventArgs e)
 	{
 
@@ -262,7 +363,10 @@ public class EditableText : MonoBehaviour {
 		SetInfo (isSelected);
 	}
 
-
+	/// <summary>
+	/// Sets the info according to selected value.
+	/// </summary>
+	/// <param name="selected">If set to <c>true</c> selected.</param>
 	void SetInfo (bool selected)
 	{
 		if (selected) {
@@ -302,7 +406,6 @@ public class EditableText : MonoBehaviour {
 	private Hashtable dict; //map character -> width
 
 	new private Renderer renderer;
-	new BoxCollider2D collider;
 	string lastText = "";
 	public void Start ()
 	{
@@ -313,14 +416,6 @@ public class EditableText : MonoBehaviour {
 		getSpace ();
 		
 	}
-	/*
-	public TextSize(TextMesh tm){
-		textMesh = tm;
-		renderer = tm.renderer;
-		dict = new Hashtable();
-		getSpace();
-	}
-	*/
 	
 	void MeasureText(string text)
 	{
@@ -396,8 +491,7 @@ public class EditableText : MonoBehaviour {
 		return w;
 	}
 	
-	public float width { get { return GetTextWidth(textMesh.text); } }
-	public float height { get { return renderer.bounds.size.y; } }
+
 
 	#endregion
 
